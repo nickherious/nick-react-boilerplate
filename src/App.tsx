97776c1a72926f7ef5b7629f0590f111f210cab2
Nicholas Herious <nick.herious@e360health.ca>
2023-12-08 15:28:45 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/style.css';
+function App() {
+  return (
+    <div>
+      <h1>Nick's React Boilerplate</h1>
+    </div>
+  );
+}
+
+export default App;
