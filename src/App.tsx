b79b83207ed5ee7c167677364766a804b4f68911
Nicholas Herious <nick.herious@e360health.ca>
2023-12-09 13:12:48 -0800

    change homepage styles and text

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './assets/styles/style.css';
 function App() {
   return (
-    <div>
+    <div className="centered-div">
       <h1>Nick's React Boilerplate</h1>
-      <h2>Just testing a new title</h2>
+      <h3>Welcome to My React Boilerplate! This project provides a starting point for building React applications with TypeScript, webpack, vitest and other essential tools.</h3>
     </div>
   );
 }
